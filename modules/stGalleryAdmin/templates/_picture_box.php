6c47badc6df0cf93bab4d8ff6c9bd0d8cd8d13d7
Miloslav Kmet <miloslav.kmet@gmail.com>
2010-11-12 11:45:05 +0100

    Added title to move action

--- modules/stGalleryAdmin/templates/_picture_box.php
+++ modules/stGalleryAdmin/templates/_picture_box.php
@@ -1,7 +1,7 @@
 <?php use_helper('I18N'); ?>
 <ul class='picture_actions'>
     <!-- li class='sf_admin_action_edit'><?php echo __('Edit', array(), 'st_gallery'); ?></li -->
-    <li class='move'><a><?php echo __('Move', array(), 'st_gallery'); ?></a></li>
+    <li class='move'><a title='<?php echo __('Move', array(), 'st_gallery'); ?>'><?php echo __('Move', array(), 'st_gallery'); ?></a></li>
     <li class='delete'><?php echo link_to(__('Delete', array(), 'st_gallery'), $route->getRouteForDeletePicture(), array('id' => $route->getObject()->getId(), 'picture' => $picture['id']), array('title' => __('Delete', array(), 'st_gallery'))); ?></li>
 </ul>
 <?php  /*<h3><?php echo __('Image %%id%%', array('%%id%%' => $picture['id'], '%%caption%%' => $picture['caption']), 'st_gallery');?></h3> */ ?>
